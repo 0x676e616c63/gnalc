75bf3de681f9140855bbc96c776fd3e4c46bd80a
panda <sh_zhaoo@qq.com>
2024-11-20 22:00:22 +0800

    add c style frontend source code

--- include/ir/function.h
+++ include/ir/function.h
@@ -3,11 +3,12 @@
 #include "base.h"
 #include "basic_block.h"
 #include "instruction.h"
+#include <memory>
 
 namespace IR {
 /**
  * @todo 剩余的处理vector的函数
- * @todo 继承User修改
+ * @todo 继承User还是Value?
  * @todo caller, callee...
  */
 class Function : public Value {
@@ -17,14 +18,14 @@ private:
      */
     std::vector<std::unique_ptr<Value>> params;
     std::vector<std::unique_ptr<BasicBlock>> blks;
-    std::vector<std::unique_ptr<Instruction>> tmp_insts; // 基本块划分前的过渡，后续可能再修改
+    std::vector<std::unique_ptr<Instruction>> insts; // 基本块划分前的过渡，后续可能再修改
 public:
     // 默认void类型
     Function(NameParam name = "") : Value(VOID, name) { }
 
     void setRtnType(_type ty) { setType(ty); }
     _type getRtnType() { return getType(); }
-    bool isDecl() { return blks.empty()&&tmp_insts.empty(); }
+    bool isDecl() { return blks.empty() && insts.empty(); }
 
     void addParam(std::unique_ptr<Value> param);
     void addBlock(std::unique_ptr<BasicBlock> blk);
--- lib/ir/function.cpp
+++ lib/ir/function.cpp
@@ -1,6 +1,16 @@
-
 #include "../../include/ir/base.h"
+#include "../../include/ir/function.h"
 
 namespace IR {
+    void Function::addParam(std::unique_ptr<Value> param) {
+        params.push_back(std::move(param));
+    }
 
+    void Function::addBlock(std::unique_ptr<BasicBlock> blk) {
+        blks.push_back(std::move(blk));
+    }
+
+    void Function::addInst(std::unique_ptr<Instruction> inst) {
+        insts.push_back(std::move(inst));
+    }
 }
