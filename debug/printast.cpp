37bb74ffc543559a0451505f79915ab0bba893fd
3033680748 <3033680748@qq.com>
2024-12-27 10:38:18 +0800

    try to compile

---
+++ debug/printast.cpp
@@ -0,0 +1,19 @@
+#include "../include/parser/ast.hpp"
+#include "../include/parser/visitor.hpp"
+#include "../include/parser/parser.hpp"
+
+std::shared_ptr<AST::CompUnit> node = nullptr;
+
+int main(int argc, char **argv){
+    yy::parser parser;
+    // parser.set_debug_level (1);
+    if (parser.parse()) {
+        std::cerr << "Parser Error" << std::endl;
+        return 1;
+    }
+
+    AST::Printer printer;
+    printer.visit(*node);
+
+    return 0;
+}
---
+++ lib/opt/live_var_analyse.cpp
@@ -0,0 +1,5 @@
+/**
+ * @brief live varable analysis
+ * @brief 对instruction分析，记录livein, liveout.
+ * @brief basicblock对其包含的instruction.begin->livein, end->liveout做转发
+ */
