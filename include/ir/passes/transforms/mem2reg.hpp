01dae4fd8c04aafb569503a7e4e5520b9cb33c36
panda <sh_zhaoo@qq.com>
2025-02-04 02:44:44 +0800

    完善了一部分mem2reg.

--- include/ir/passes/transforms/mem2reg.hpp
+++ include/ir/passes/transforms/mem2reg.hpp
@@ -11,24 +11,34 @@
 #include <vector>
 
 #include "../../visitor.hpp"
+#include "../analysis/domtree_analysis.hpp"
 
 namespace IR {
 class PromotePass : public PM::PassInfo<PromotePass> {
-    // ...
-    struct BLOCK_INFO {};
+    struct BLOCK_INFO {
+        std::map<unsigned, std::shared_ptr<LOADInst>> load_map;
+        std::map<unsigned, std::shared_ptr<STOREInst>> store_map;
+    };
     struct ALLOCA_INFO {
         std::shared_ptr<ALLOCAInst> alloca;
         std::vector<std::shared_ptr<LOADInst>> loads;
         std::vector<std::shared_ptr<STOREInst>> stores;
+        std::map<std::shared_ptr<BasicBlock>, BLOCK_INFO> user_blocks; // load, store的父块信息map
     };
     std::queue<ALLOCA_INFO> alloca_infos;
-    // std::shared_ptr<Function> cur_func;
     std::shared_ptr<BasicBlock> entry_block;
+    DomTree DT;
+    ALLOCA_INFO cur_info;
+
+    // 用于判断INST的支配关系
+    bool iADomB(const std::shared_ptr<Instruction>& ia, const std::shared_ptr<Instruction>& ib);
 
     void analyseAlloca();
     bool removeUnusedAlloca();
     bool rewriteSingleStoreAlloca();
     bool promoteSingleBlockAlloca();
+    void computeProcessQueue();
+    void processAlloca();
 
     void promoteMemoryToRegister(Function &function);
 
--- lib/ir/passes/transforms/mem2reg.cpp
+++ lib/ir/passes/transforms/mem2reg.cpp
@@ -2,6 +2,13 @@
 #include "../../../../include/utils/exception.hpp"
 
 namespace IR {
+    bool PromotePass::iADomB(const std::shared_ptr<Instruction>& ia, const std::shared_ptr<Instruction>& ib) {
+        if (ia->getParent() == ib->getParent()) {
+            return ia->getParent()->getInstIndex(ia) < ib->getParent()->getInstIndex(ib);
+        }
+        return DT.ADomB(ia->getParent(), ib->getParent());
+    }
+
 void PromotePass::analyseAlloca() {
     for (const auto &inst : entry_block->getInsts()) {
         if (inst->getOpcode() == OP::ALLOCA) {
@@ -12,21 +19,40 @@ void PromotePass::analyseAlloca() {
             // 遍历所有User, 只接受LOAD, STORE语句
             for (const auto &use : inst->getUseList()) {
                 const auto user = use->getUser()->shared_from_this();
-                // 这里前提是所有的ORDINARY_VARIABLE都是INSTRUCTION
-                if (user->getVTrait() == ValueTrait::ORDINARY_VARIABLE &&
-                    std::dynamic_pointer_cast<Instruction>(user)->getOpcode() ==
-                        OP::LOAD) {
-                    info.loads.emplace_back(
-                        std::dynamic_pointer_cast<LOADInst>(user));
-                } else if (user->getVTrait() == ValueTrait::VOID_INSTRUCTION &&
-                           std::dynamic_pointer_cast<Instruction>(user)
-                                   ->getOpcode() == OP::STORE) {
-                    info.stores.emplace_back(
-                        std::dynamic_pointer_cast<STOREInst>(user));
+                // Attention: 这里前提是所有的ORDINARY_VARIABLE都是INSTRUCTION
+                if (user->getVTrait() == ValueTrait::ORDINARY_VARIABLE ||
+                    user->getVTrait() == ValueTrait::VOID_INSTRUCTION) {
+                    auto inst_user = std::dynamic_pointer_cast<Instruction>(user);
+                    if (inst_user->getOpcode() == OP::LOAD) {
+                        auto load = std::dynamic_pointer_cast<LOADInst>(inst_user);
+                        info.loads.emplace_back(load);
+                        info.user_blocks[load->getParent()].load_map[inst_user->getParent()->getInstIndex(inst_user)] = load;
+                    } else if (inst_user->getOpcode() == OP::STORE) {
+                        auto store = std::dynamic_pointer_cast<STOREInst>(inst_user);
+                        info.stores.emplace_back(store);
+                        info.user_blocks[store->getParent()].store_map[inst_user->getParent()->getInstIndex(inst_user)] = store;
+                    } else {
+                        promotable = false;
+                        break;
+                    }
                 } else {
                     promotable = false;
                     break;
                 }
+                // if (user->getVTrait() == ValueTrait::ORDINARY_VARIABLE &&
+                //     std::dynamic_pointer_cast<Instruction>(user)->getOpcode() ==
+                //         OP::LOAD) {
+                //     info.loads.emplace_back(
+                //         std::dynamic_pointer_cast<LOADInst>(user));
+                // } else if (user->getVTrait() == ValueTrait::VOID_INSTRUCTION &&
+                //            std::dynamic_pointer_cast<Instruction>(user)
+                //                    ->getOpcode() == OP::STORE) {
+                //     info.stores.emplace_back(
+                //         std::dynamic_pointer_cast<STOREInst>(user));
+                // } else {
+                //     promotable = false;
+                //     break;
+                // }
             }
             if (promotable) {
                 alloca_infos.push(info);
@@ -36,7 +62,7 @@ void PromotePass::analyseAlloca() {
 }
 
 bool PromotePass::removeUnusedAlloca() {
-    if (auto cur_info = alloca_infos.front(); cur_info.loads.empty()) {
+    if (cur_info.loads.empty()) {
         for (auto &store : cur_info.stores) {
             store->getParent()->delFirstOfInst(store);
         }
@@ -46,16 +72,44 @@ bool PromotePass::removeUnusedAlloca() {
     return false;
 }
 
-// todo: store前load
 bool PromotePass::rewriteSingleStoreAlloca() {
-    if (auto cur_info = alloca_infos.front(); cur_info.stores.size() == 1) {
-        std::shared_ptr<Value> store_val = cur_info.stores.front()->getValue();
+    if (cur_info.stores.size() == 1) {
+        auto store = cur_info.stores.front();
+        auto rval = store->getValue();
         for (auto &load : cur_info.loads) {
-            // for (const auto & use : load->getUseList()) {
-            //     use->getUser()->replaceUse(load, store_val);
-            // }
-            load->replaceSelf(store_val);
+            if (!iADomB(store, load)) {
+                // todo: 替换为undef?
+                Err::error("PromotePass::rewriteSingleStoreAlloca(): load before single store.");
+                return false;
+            }
+            load->replaceSelf(rval);
             load->getParent()->delFirstOfInst(load);
+        }
+        store->getParent()->delFirstOfInst(store);
+        entry_block->delFirstOfInst(cur_info.alloca);
+        return true;
+    }
+    return false;
+}
+
+bool PromotePass::promoteSingleBlockAlloca() {
+    if (cur_info.user_blocks.size() == 1) {
+        for (auto & load : cur_info.loads) {
+            auto it =  cur_info.user_blocks[load->getParent()].store_map.lower_bound(load->getParent()->getInstIndex(load));
+            if (it == cur_info.user_blocks[load->getParent()].store_map.begin()) {
+                Err::error("PromotePass::promoteSingleBlockAlloca(): store map is empty or load before store.");
+                return false;
+            }
+            auto rval = (--it)->second->getValue();
+            if (rval == load) {
+                Err::error("PromotePass::promoteSingleBlockAlloca(): Unreachable load after promoting store.");
+                return false;
+            }
+            load->replaceSelf(rval);
+            load->getParent()->delFirstOfInst(load);
+        }
+        for (auto &store : cur_info.stores) {
+            store->getParent()->delFirstOfInst(store);
         }
         entry_block->delFirstOfInst(cur_info.alloca);
         return true;
@@ -63,7 +117,48 @@ bool PromotePass::rewriteSingleStoreAlloca() {
     return false;
 }
 
-bool PromotePass::promoteSingleBlockAlloca() { return false; }
+void PromotePass::computeProcessQueue() {
+    std::queue<std::shared_ptr<BasicBlock>> tmp_work_queue; // 临时处理队列
+    std::set<std::shared_ptr<BasicBlock>> live_in_blocks; // 即需要传递alloca值的块
+
+    // 初步处理 user_block
+    for (const auto &[b, i] : cur_info.user_blocks) {
+        if (!i.load_map.empty()) {
+            if (!i.store_map.empty()) {
+                if (i.store_map.begin()->first < i.load_map.begin()->first) {
+                    // store's index is less than load's
+                    continue;
+                }
+            }
+            tmp_work_queue.push(b);
+        }
+    }
+
+    // 进一步递归向前遍历前驱构造 live_in_blocks
+    while (!tmp_work_queue.empty()) {
+        auto b = tmp_work_queue.front();
+        tmp_work_queue.pop();
+
+        if (!live_in_blocks.insert(b).second)
+            continue;
+
+        for (const auto &p : b->getPreBB()) {
+            if (auto i = cur_info.user_blocks.find(p);
+                i != cur_info.user_blocks.end()) {
+                if (!i->second.store_map.empty())
+                    // if p is a def block...
+                    continue;
+            }
+            tmp_work_queue.push(p);
+        }
+    }
+    // todo
+
+}
+
+void PromotePass::processAlloca() {
+        // todo
+}
 
 void PromotePass::promoteMemoryToRegister(Function &function) {
     entry_block = function.getBlocks().front();
@@ -71,18 +166,22 @@ void PromotePass::promoteMemoryToRegister(Function &function) {
                  "First block is not named entry");
 
     analyseAlloca();
-
     while (!alloca_infos.empty()) {
-        if (removeUnusedAlloca()) {
-        } else if (rewriteSingleStoreAlloca()) {
+        cur_info = alloca_infos.front();
+        if (removeUnusedAlloca()) ;
+        else if (rewriteSingleStoreAlloca()) ;
+        else if (promoteSingleBlockAlloca()) ;
+        else {
+            computeProcessQueue();
+            processAlloca();
         }
-        // todo
         alloca_infos.pop();
     }
 }
 
 PM::PreservedAnalyses PromotePass::run(Function &function, FAM &manager) {
+    DT = {/*todo*/};
     promoteMemoryToRegister(function);
-    // todo
+    return {/*todo*/};
 }
 } // namespace IR
