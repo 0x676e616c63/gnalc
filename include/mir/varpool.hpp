5df298d0b82d4b0958fdec81dc3274af94a66694
3033680748 <3033680748@qq.com>
2025-03-19 13:10:43 +0800

    stage

--- include/mir/varpool.hpp
+++ include/mir/varpool.hpp
@@ -22,21 +22,18 @@ private:
         }
     };
 
-    std::unordered_map<IRValueWrapper, std::shared_ptr<Operand>, VarPoolHash>
-        pool;
+    std::unordered_map<IRValueWrapper, std::shared_ptr<Operand>, VarPoolHash> pool;
 
     /// 一个ConstObj, 经过ldr/mov之后得到的虚拟寄存器
     /// @warning 理论上, 这个东西可以减少ldr/mov的数量, 但是可能增加寄存器压力
     /// @warning , 尤其是某个常数或者全局变量很常用, 而且执行流程又很长时
     /// @warning 然后, 一些特别大的常数可能不在这里面, 比如ChooseMultipler生成的multipler
+    /// @warning with complex CFGs, a ConstIDX maybe used before def.
     /// @note 其次, 这个东西可以被其他优化方法取代
     struct LoadMapHash {
-        size_t operator()(const ConstObj &obj) const {
-            return (size_t)obj.getId();
-        }
+        size_t operator()(const ConstObj &obj) const { return (size_t)obj.getId(); }
     };
-    std::unordered_map<ConstObj, std::shared_ptr<BindOnVirOP>, LoadMapHash>
-        load_map;
+    std::unordered_map<ConstObj, std::shared_ptr<BindOnVirOP>, LoadMapHash> load_map;
 
     // 两类预着色操作数
     std::unordered_map<CoreRegister, std::shared_ptr<PreColedOP>> gpr_pool;
@@ -51,11 +48,11 @@ public:
     std::shared_ptr<PreColedOP> getValue(FPURegister _color);
 
     template <typename T_variant>
-    std::pair<std::shared_ptr<BindOnVirOP>, std::shared_ptr<ConstObj>>
-    getLoaded(const T_variant &literal) {
+    std::pair<std::shared_ptr<BindOnVirOP>, std::shared_ptr<ConstObj>> getLoaded(const T_variant &literal) {
         unsigned int cnt = 0;
         for (const auto &pair : load_map) {
-            if (literal == std::get<std::remove_cv_t<std::remove_reference_t<decltype(literal)>>>(pair.first.getLiteral()))
+            if (literal ==
+                std::get<std::remove_cv_t<std::remove_reference_t<decltype(literal)>>>(pair.first.getLiteral()))
                 return {load_map[pair.first], nullptr};
             ++cnt;
         }
@@ -66,16 +63,15 @@ public:
         return {op, obj};
     }
 
-    std::shared_ptr<BindOnVirOP> getLoaded(const ConstObj &obj) {
-        return load_map[obj];
-    }
+    std::shared_ptr<BindOnVirOP> getLoaded(const ConstObj &obj) { return load_map[obj]; }
 
     void addValue(const IR::Value &, std::shared_ptr<Operand>); // Def
 
     std::shared_ptr<BindOnVirOP> addValue_anonymously(bool isFloat); // 用于添加一个新的BindOnVirOP
 
-    std::shared_ptr<StackADROP> addStackValue_anonymously(const std::shared_ptr<FrameObj> &); // 用于获得一个空的栈空间(4bytes)
-                                                                                              // 寄存器分配用
+    std::shared_ptr<StackADROP>
+    addStackValue_anonymously(const std::shared_ptr<FrameObj> &); // 用于获得一个空的栈空间(4bytes)
+                                                                  // 寄存器分配用
     void addLoaded(const ConstObj &, std::shared_ptr<BindOnVirOP>);
     size_t size() const { return pool.size(); }
 
