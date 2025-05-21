41e457449d5f0961431439b9a68379ea03bb94ea
caozhanhao <cao2013zh@163.com>
2025-05-21 18:20:58 +0800

    IR: Implement part of RangeAnalysis.

--- include/ir/passes/analysis/range_analysis.hpp
+++ include/ir/passes/analysis/range_analysis.hpp
@@ -11,24 +11,25 @@
 
 namespace IR {
 // [ min, max )
+template <typename T>
 struct Range {
-    ConstantProxy min;
-    ConstantProxy max;
+    static constexpr auto MAX = std::numeric_limits<T>::max();
+    static constexpr auto MIN = std::numeric_limits<T>::min();
 
-    Range(const ConstantProxy &a) : min(a), max(a) {}
-    Range(ConstantProxy min_, ConstantProxy max_) : min(std::move(min_)), max(std::move(max_)) {}
+    T min;
+    T max;
+
+    Range(): min(MIN), max(MAX) {}
+    Range(T a) : min(a), max(a) {}
+    Range(T min_, T max_) : min(min_), max(max_) {}
 
     bool overlap(const Range &item) const { return min <= item.max && max >= item.min; }
 
-    std::optional<ConstantProxy> getExactConstant() const {
+    std::optional<T> getExact() const {
         if (min == max)
             return min;
         return std::nullopt;
     }
-
-    std::pair<int, int> getIntRange() const { return {min.get_int(), max.get_int()}; }
-
-    std::pair<float, float> getFloatRange() const { return {min.get_float(), max.get_float()}; }
 
     bool operator==(const Range &item) const { return min == item.min && max == item.max; }
 
@@ -36,43 +37,147 @@ struct Range {
     Range operator-(const Range &item) const { return {min - item.max, max - item.min}; }
     Range operator-() const { return {-max, -min}; }
     Range operator*(const Range &item) const {
-        auto p = std::min(std::min(min * item.min, max * item.min), std::min(min * item.max, max * item.max));
-        auto q = std::max(std::max(min * item.min, max * item.min), std::max(min * item.max, max * item.max));
+        auto p = (std::min)((std::min)(min * item.min, max * item.min), (std::min)(min * item.max, max * item.max));
+        auto q = (std::max)((std::max)(min * item.min, max * item.min), (std::max)(min * item.max, max * item.max));
         return {p, q};
     }
     Range operator/(const Range &item) const {
-        auto p = std::min(std::min(min / item.min, max / item.min), std::min(min / item.max, max / item.max));
-        auto q = std::max(std::max(min / item.min, max / item.min), std::max(min / item.max, max / item.max));
+        auto p = (std::min)((std::min)(min / item.min, max / item.min), (std::min)(min / item.max, max / item.max));
+        auto q = (std::max)((std::max)(min / item.min, max / item.min), (std::max)(min / item.max, max / item.max));
         return {p, q};
+    }
+    Range operator%(const Range &item) const {
+        // FIXME: Precise
+        return Range();
     }
 
     bool containsZero() const {
-        if (auto min_ci32 = min.getConstant()->as<ConstantInt>()) {
-            auto max_ci32 = max.getConstant()->as<ConstantInt>();
-            return min_ci32->getVal() <= 0 && max_ci32->getVal() >= 0;
-        }
-        if (auto min_cf32 = min.getConstant()->as<ConstantFloat>()) {
-            auto max_cf32 = max.getConstant()->as<ConstantFloat>();
-            return min_cf32->getVal() <= 0.0f && max_cf32->getVal() >= 0.0f;
-        }
-        if (auto min_ci8 = min.getConstant()->as<ConstantI8>()) {
-            auto max_ci8 = max.getConstant()->as<ConstantI8>();
-            return min_ci8->getVal() <= 0 && max_ci8->getVal() >= 0;
-        }
-        if (auto min_ci1 = min.getConstant()->as<ConstantI1>()) {
-            auto max_ci1 = max.getConstant()->as<ConstantI1>();
-            return !min_ci1->getVal() || !max_ci1->getVal();
-        }
+        return min <= T(0) && max >= T(0);
+    }
+
+    bool merge(const Range &item) {
+        if (*this == item)
+            return false;
+        min = (std::min)(min, item.min);
+        max = (std::max)(max, item.max);
         return true;
+    }
+    bool intersect(const Range &item) {
+        if (*this == item)
+            return false;
+        min = (std::max)(min, item.min);
+        max = (std::min)(max, item.max);
+        return true;
+    }
+};
+template  <typename T>
+Range<T> merge(const Range<T> &a, const Range<T> &b) {
+    return {(std::min)(a.min, b.min), (std::max)(a.max, b.max)};
+}
+
+template <typename T>
+Range<T> intersect(const Range<T> &a, const Range<T> &b){
+    return {(std::max)(a.min, b.min), (std::min)(a.max, b.max)};
+}
+
+template <typename T>
+struct ContextualRange {
+    friend class RangeResult;
+private:
+    std::unordered_map<BasicBlock*, Range<T>> context_map;
+    Range<T> global;
+
+    bool updateGlobal(const Range<T> &range) {
+        return global.intersect(range);
+    }
+    bool updateContextual(const Range<T> &range, BasicBlock* bb) {
+        return context_map[bb].intersect(range);
+    }
+public:
+    const Range<T>& getContextual(BasicBlock* bb) const {
+        auto it = context_map.find(bb);
+        if (it == context_map.end())
+            return global;
+        return it->second;
+    }
+    const Range<T>& getGlobal() const {
+        return global;
     }
 };
 
 class RangeResult {
-    std::unordered_map<pVal, Range> range_map;
+    friend class RangeAnalysis;
+
+    std::unordered_map<Value*, ContextualRange<int>> int_range_map;
+    std::unordered_map<Value*, ContextualRange<float>> float_range_map;
 public:
     RangeResult() = default;
 
-    const Range& getRange(const pVal &val) const { return range_map.at(val); }
+    const Range<int>& getIntRange(Value* val) const {
+        return int_range_map.at(val).getGlobal();
+    }
+    const Range<int>& getIntRange(const pVal &val) const {
+        return getIntRange(val.get());
+    }
+
+    const Range<int>& getIntRange(Value* val, BasicBlock* bb) const {
+        return int_range_map.at(val).getContextual(bb);
+    }
+    const Range<int>& getIntRange(const pVal &val, const pBlock& bb) const {
+        return getIntRange(val.get(), bb.get());
+    }
+
+    const Range<float>& getFloatRange(Value* val) const {
+        return float_range_map.at(val).getGlobal();
+    }
+    const Range<float>& getFloatRange(const pVal &val) const {
+        return getFloatRange(val.get());
+    }
+
+    const Range<float>& getFloatRange(Value* val, BasicBlock* bb) const {
+        return float_range_map.at(val).getContextual(bb);
+    }
+    const Range<float>& getFloatRange(const pVal &val, const pBlock& bb) const {
+        return getFloatRange(val.get(), bb.get());
+    }
+
+private:
+    bool update(Value* val, const Range<int> &range) {
+        return int_range_map[val].updateGlobal(range);
+    }
+    bool update(Value* val, const Range<int> &range, BasicBlock* bb) {
+        if (auto inst = val->as_raw<Instruction>()) {
+            if (inst->getParent().get() == bb) {
+                return update(inst, range);
+            }
+        }
+        return int_range_map[val].updateContextual(range, bb);
+    }
+    bool update(const pVal &val, const Range<int> &range) {
+        return update(val.get(), range);
+    }
+    bool update(const pVal &val, const Range<int> &range, const pBlock& bb) {
+        return update(val.get(), range, bb.get());
+    }
+
+    bool update(Value* val, const Range<float> &range) {
+        return float_range_map[val].updateGlobal(range);
+    }
+    bool update(Value* val, const Range<float> &range, BasicBlock* bb) {
+        if (auto inst = val->as_raw<Instruction>()) {
+            if (inst->getParent().get() == bb) {
+                return update(inst, range);
+            }
+        }
+        return float_range_map[val].updateContextual(range, bb);
+    }
+    bool update(const pVal &val, const Range<float> &range) {
+        return update(val.get(), range);
+    }
+    bool update(const pVal &val, const Range<float> &range, const pBlock& bb) {
+        return update(val.get(), range, bb.get());
+    }
+
 };
 
 class RangeAnalysis : public PM::AnalysisInfo<RangeAnalysis> {
--- lib/ir/passes/analysis/range_analysis.cpp
+++ lib/ir/passes/analysis/range_analysis.cpp
@@ -1,7 +1,12 @@
 #include "ir/passes/analysis/range_analysis.hpp"
 
 #include "ir/base.hpp"
+#include "ir/instructions/binary.hpp"
+#include "ir/instructions/compare.hpp"
 #include "ir/instructions/control.hpp"
+#include "ir/instructions/converse.hpp"
+#include "ir/instructions/memory.hpp"
+#include "ir/instructions/vector.hpp"
 #include "ir/passes/analysis/live_analysis.hpp"
 #include "utils/logger.hpp"
 
@@ -11,6 +16,137 @@ namespace IR {
 PM::UniqueKey RangeAnalysis::Key;
 
 RangeResult RangeAnalysis::run(Function &func, FAM &fam) {
+    RangeResult res;
+    // Initialize Range
+    auto bbdfv = func.getDFVisitor();
+
+    // Argument
+    for (const auto &inst_user : func.inst_users()) {
+        auto call = inst_user->as<CALLInst>();
+        Err::gassert(call != nullptr);
+        auto caller_func = call->getParent()->getParent();
+        if (caller_func.get() != &func) {
+            const auto caller_res = fam.getResult<RangeAnalysis>(*caller_func);
+            auto actual_args = call->getArgs();
+            for (const auto &fp : func.getParams()) {
+                if (fp->getType()->is<BType>()) {
+                    auto btype = fp->getType()->as<BType>()->getInner();
+                    if (btype == IRBTYPE::I32)
+                        res.update(fp, caller_res.getIntRange(actual_args[fp->getIndex()]));
+                    else if (btype == IRBTYPE::FLOAT)
+                        res.update(fp, caller_res.getFloatRange(actual_args[fp->getIndex()]));
+                    else
+                        Err::unreachable();
+                }
+            }
+        }
+    }
+
+    // Instructions
+    for (const auto &bb : bbdfv) {
+        for (const auto &inst : *bb) {
+            bool is_btype = inst->getType()->is<BType>();
+            bool is_int = is_btype && inst->getType()->as<BType>()->getInner() == IRBTYPE::I32;
+
+            if (auto binary = inst->as<BinaryInst>()) {
+                if (is_int) {
+                    auto lrng = res.getIntRange(binary->getLHS());
+                    auto rrng = res.getIntRange(binary->getRHS());
+                    if (binary->getOpcode() == OP::ADD)
+                        res.update(binary, lrng + rrng);
+                    else if (binary->getOpcode() == OP::SUB)
+                        res.update(binary, lrng - rrng);
+                    else if (binary->getOpcode() == OP::MUL)
+                        res.update(binary, lrng * rrng);
+                    else if (binary->getOpcode() == OP::DIV)
+                        res.update(binary, lrng / rrng);
+                    else if (binary->getOpcode() == OP::REM)
+                        res.update(binary, lrng % rrng);
+                    else
+                        Err::unreachable();
+                } else {
+                    auto lrng = res.getFloatRange(binary->getLHS());
+                    auto rrng = res.getFloatRange(binary->getRHS());
+                    if (binary->getOpcode() == OP::FADD)
+                        res.update(binary, lrng + rrng);
+                    else if (binary->getOpcode() == OP::FSUB)
+                        res.update(binary, lrng - rrng);
+                    else if (binary->getOpcode() == OP::FMUL)
+                        res.update(binary, lrng * rrng);
+                    else if (binary->getOpcode() == OP::FDIV)
+                        res.update(binary, lrng / rrng);
+                    else if (binary->getOpcode() == OP::FREM)
+                        res.update(binary, lrng % rrng);
+                    else
+                        Err::unreachable();
+                }
+            } else if (auto fneg = inst->as<FNEGInst>()) {
+                const auto &vrng = res.getFloatRange(fneg->getVal());
+                res.update(fneg, -vrng);
+            } else if (auto fptosi = inst->as<FPTOSIInst>()) {
+                const auto &vrng = res.getFloatRange(fptosi->getOVal());
+                res.update(fptosi, Range(static_cast<int>(vrng.min), static_cast<int>(vrng.max)));
+            } else if (auto sitofp = inst->as<SITOFPInst>()) {
+                const auto &vrng = res.getIntRange(sitofp->getOVal());
+                res.update(sitofp, Range(static_cast<float>(vrng.min), static_cast<float>(vrng.max)));
+            } else if (auto zext = inst->as<ZEXTInst>()) {
+                // TODO: I8 Range?
+                const auto &vrng = res.getIntRange(zext->getOVal());
+                res.update(zext, vrng);
+            } else if (auto phi = inst->as<PHIInst>()) {
+                auto phi_opers = phi->getPhiOpers();
+                if (is_int) {
+                    auto base = res.getIntRange(phi_opers[0].value);
+                    for (size_t i = 1; i < phi_opers.size(); i++)
+                        base = merge(base, res.getIntRange(phi_opers[i].value));
+                    res.update(phi, base);
+                } else {
+                    auto base = res.getFloatRange(phi_opers[0].value);
+                    for (size_t i = 1; i < phi_opers.size(); i++)
+                        base = merge(base, res.getFloatRange(phi_opers[i].value));
+                    res.update(phi, base);
+                }
+            } else if (auto select = inst->as<SELECTInst>()) {
+                if (is_int) {
+                    auto trng = res.getIntRange(select->getTrueVal());
+                    auto frng = res.getIntRange(select->getFalseVal());
+                    res.update(select, merge(trng, frng));
+                } else {
+                    auto trng = res.getFloatRange(select->getTrueVal());
+                    auto frng = res.getFloatRange(select->getFalseVal());
+                    res.update(select, merge(trng, frng));
+                }
+            } else if (auto call = inst->as<CALLInst>()) {
+                if (is_int && call->getFunc()->hasAttr(FuncAttr::PromoteFromChar))
+                    res.update(call, Range<int>(0, 256));
+                else {
+                    if (is_int)
+                        res.update(call, Range<int>());
+                    else
+                        res.update(call, Range<float>());
+                }
+            } else if (auto gep = inst->as<GEPInst>()) {
+                auto idxs = gep->getIdxs();
+                pType curr_type = gep->getBaseType();
+                for (const auto &idx : idxs) {
+                    auto irng = res.getIntRange(idx);
+                    auto max_v = static_cast<int>(curr_type->as<ArrayType>()->getArraySize());
+                    res.update(idx, intersect(irng, Range(0, max_v)));
+                }
+            } else if (inst->is<ICMPInst, FCMPInst>())
+                res.update(inst, Range<int>(0, 1));
+            else if (inst->is<LOADInst, EXTRACTInst>()) {
+                if (is_btype) {
+                    if (is_int)
+                        res.update(inst, Range<int>());
+                    else
+                        res.update(inst, Range<float>());
+                }
+            }
+        }
+    }
+
+    // Analyze Constraints
     return RangeResult{};
 }
 } // namespace IR
