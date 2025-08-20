e45079593f9135b7f3dc42e4052b2284b05ccbcb
caozhanhao <cao2013zh@163.com>
2025-08-20 14:06:00 +0800

    OJ: Fix RangeAnaly [skip ci]

--- lib/ir/passes/analysis/range_analysis.cpp
+++ lib/ir/passes/analysis/range_analysis.cpp
@@ -706,7 +706,7 @@ void RangeAnalysis::analyzeContextual(RangeResult &res, Function *func, FAM *fam
                         int c;
                         if (trip_count->isIRValue() && match(trip_count->getIRValue(), M::Bind(c))) {
                             auto m = static_cast<IRng::Bigger>(base) +
-                                     static_cast<IRng::Bigger>(step) * static_cast<IRng::Bigger>(c - 1);
+                                     static_cast<IRng::Bigger>(step) * static_cast<IRng::Bigger>(c);
                             if (step > 0)
                                 return IRng(base, m);
                             return IRng(m, base);
@@ -732,37 +732,12 @@ void RangeAnalysis::analyzeContextual(RangeResult &res, Function *func, FAM *fam
                 return IRng();
             };
 
-            auto analyzePeeledTREC = [&scev, &analyzeSCEVExpr, &analyzeAddRec](TREC *trec) {
-                auto first_rng = analyzeSCEVExpr(trec->getFirst());
-                if (auto trip_count = scev.getTripCount(trec->getLoop())) {
-                    int trip_cnt_ci;
-                    if (trip_count->isIRValue() && match(trip_count->getIRValue(), M::Bind(trip_cnt_ci))) {
-                        if (trip_cnt_ci <= 1)
-                            return first_rng;
-                    }
-                }
-
-                auto rest = trec->getRest();
-                if (rest->isExpr()) {
-                    auto rng = analyzeSCEVExpr(rest->getExpr());
-                    return merge(IRng(first_rng), rng);
-                }
-                if (rest->isAddRec()) {
-                    auto rng = analyzeAddRec(rest);
-                    return merge(IRng(first_rng), rng);
-                }
-                return IRng();
-            };
-
             auto trec = scev.getSCEVAtBlock(inst, bb);
             if (trec->isExpr()) {
                 if (intersectContextualInt(inst, bb, analyzeSCEVExpr(trec->getExpr())))
                     continue;
             } else if (trec->isAddRec()) {
                 if (intersectContextualInt(inst, bb, analyzeAddRec(trec)))
-                    continue;
-            } else if (trec->isPeeled()) {
-                if (intersectContextualInt(inst, bb, analyzePeeledTREC(trec)))
                     continue;
             }
         }
